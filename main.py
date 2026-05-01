f2e40dbe32b329882c997495d0ef72b50a5034f3
dano200542 <dano200542@gmail.com>
2026-05-01 19:47:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+print("🔥 SCRIPT IS RUNNING")
+
 import requests
 
-API_KEY = "GCAqzGwvpveABEjg92lZszGhAtbGJVYp"
+API_KEY = "TEST"
 
-symbol = "AAPL"
-
-url = f"https://api.massive.com/v1/quote/{symbol}?apikey={API_KEY}"
+url = "https://httpbin.org/get"
 
 r = requests.get(url)
 
 print("STATUS CODE:", r.status_code)
-print("RAW RESPONSE:")
-print(r.text)
+print("RESPONSE OK")
