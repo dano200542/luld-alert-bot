7b65f751c87ba168aaefe59a14a71bc9197066c7
dano200542 <dano200542@gmail.com>
2026-05-01 19:34:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import requests
-import time
 
 API_KEY = "GCAqzGwvpveABEjg92lZszGhAtbGJVYp"
 WEBHOOK = "https://discord.com/api/webhooks/1463521320582250498/Oi0KYpKQzUVYLnRIKGSToqnrkfEd1A-BIPnDz5bmW7gsi5T46CrTzO7u2Eu1AuK1IoLA"
 
+def test_api():
+    url = f"https://api.massive.com/v1/quote/AAPL?apikey={API_KEY}"
+    
+    try:
+        r = requests.get(url, timeout=10)
+        print("STATUS:", r.status_code)
+        print("TEXT:", r.text[:300])
+    except Exception as e:
+        print("ERROR:", e)
 
-SYMBOLS = ["AAPL", "TSLA", "NVDA"]
-
-def alert(msg):
-    requests.post(WEBHOOK, json={"content": msg})
-
-while True:
-    print("checking market...")
-
-    for symbol in SYMBOLS:
-        url = f"https://api.massive.com/v1/quote/{symbol}?apikey={API_KEY}"
-        data = requests.get(url).json()
-
-        print(symbol, data)
-
-    time.sleep(60)
+if __name__ == "__main__":
+    test_api()
