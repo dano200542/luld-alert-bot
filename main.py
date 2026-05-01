f45513a7df716ddc0c1ff92d2f8c3c9428609cb7
dano200542 <dano200542@gmail.com>
2026-05-01 14:58:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
+import os
 import time
 import requests
 from flask import Flask
 from threading import Thread
-import os
 
 app = Flask(__name__)
 
-# 🔥 YOUR CURRENT WEBHOOK (DO NOT CHANGE UNLESS IT BREAKS)
 WEBHOOK_URL = "https://discord.com/api/webhooks/1463521320582250498/Oi0KYpKQzUVYLnRIKGSToqnrkfEd1A-BIPnDz5bmW7gsi5T46CrTzO7u2Eu1AuK1IoLA"
 
 @app.route("/")
 def home():
     return "OK - LIVE"
 
-def fire():
-    time.sleep(2)
+def fire_alert():
+    time.sleep(3)
 
     print("TRYING DISCORD")
 
@@ -32,7 +31,7 @@ def fire():
         print("ERROR:", e)
 
 if __name__ == "__main__":
-    Thread(target=fire, daemon=True).start()
+    Thread(target=fire_alert, daemon=True).start()
 
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
