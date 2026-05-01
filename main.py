6b9f95770175af0d5244d7c137f4becca08c859c
dano200542 <dano200542@gmail.com>
2026-05-01 19:43:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import requests
 
 API_KEY = "GCAqzGwvpveABEjg92lZszGhAtbGJVYp"
 
-SYMBOLS = ["AAPL"]
+symbol = "AAPL"
 
-for symbol in SYMBOLS:
-    url = f"https://api.massive.com/v1/quote/{symbol}?apikey={API_KEY}"
+url = f"https://api.massive.com/v1/quote/{symbol}?apikey={API_KEY}"
 
-    r = requests.get(url)
+r = requests.get(url)
 
-    print("STATUS CODE:", r.status_code)
-    print("RAW RESPONSE:")
-    print(r.text)
+print("STATUS CODE:", r.status_code)
+print("RAW RESPONSE:")
+print(r.text)
